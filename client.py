18655523d0b7595850bb5fb2428ca0c64554715f
CHURLZ <kalle.berglund@gmail.com>
2018-07-19 00:49:58 +0100

    client dump

--- client.py
+++ client.py
@@ -15,3 +15,33 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 		s.sendall(b'Hello, world')
 		data = s.recv(1024)
 		print('Received', repr(data))
+
+
+
+
+
+
+# 		receive_stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, output=True, frames_per_buffer=CHUNK)
+# send_stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
+
+# print("Voice chat running")
+
+# def receive_data():
+#     while True:
+#         try:
+#             data = s.recv(1024)
+#             receive_stream.write(data)
+#         except:
+#             pass
+
+
+# def send_data():
+#     while True:
+#         try:
+#             data = send_stream.read(CHUNK)
+#             s.sendall(data)
+#         except:
+#             pass
+
+# thread.start_new_thread(receive_data, ())
+# thread.start_new_thread(send_data, ())
